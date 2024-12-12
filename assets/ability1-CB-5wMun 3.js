458e1620f657db61680223412e9201f536ef22f2
luffythink <luffythink@gmail.com>
2024-12-12 20:29:18 +0800

    updates

---
+++ assets/ability1-CB-5wMun 3.js
@@ -0,0 +1,7 @@
+const t={date:"2024-08",Title:"无视交通规则怪",icon:"bi bi-1-circle-fill"},i=`<p>不遵守交通信号灯、不礼让行人等。</p>
+<div><img src="/assets/XX" style="" title=""/></div>
+`,s=`不遵守交通信号灯、不礼让行人等。
+
+<div><img src="/assets/XX" style="" title=""/></div>
+
+`;export{t as attributes,i as html,s as markdown};
