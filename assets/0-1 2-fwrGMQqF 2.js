fb56b905faacda5918d077a07888a82518b0eba4
luffythink <luffythink@gmail.com>
2024-12-13 13:07:34 +0800

    updates

--- assets/0-1 2-fwrGMQqF 2.js
+++
@@ -1,3 +0,0 @@
-const t={date:"2016",title:"0 到 1 学会骑车",url:"https://github.com/jason5ng32/pm2048",tags:["教学","免费"]},n=`<p>现场骑车纯粹演示 ，0元骑行教学。</p>
-`,o=`现场骑车纯粹演示 ，0元骑行教学。
-`;export{t as attributes,n as html,o as markdown};
--- assets/pump-D2KpwpCf 3.js
+++
@@ -1,2 +0,0 @@
-const t={date:"2024-01",title:"为你打气",url:"https://retire.money",tags:["维护","互动"]},e=`<p>骑行友好大旗共绘，打气筒为你打气。</p>
-`,n="骑行友好大旗共绘，打气筒为你打气。";export{t as attributes,e as html,n as markdown};
