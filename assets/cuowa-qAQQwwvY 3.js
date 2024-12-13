05a586a72d7955dcb4dde598b481f8b75e7da42a
luffythink <luffythink@gmail.com>
2024-12-13 13:02:09 +0800

    update

---
+++ assets/cuowa-qAQQwwvY 3.js
@@ -0,0 +1,3 @@
+const t={date:"2019",title:"错袜骑行",url:"XXX",tags:["公益","温暖同行"]},n=`<p>一起穿错袜，爱与包容同行。</p>
+`,o=`一起穿错袜，爱与包容同行。
+`;export{t as attributes,n as html,o as markdown};
---
+++ assets/debate 2-CJ3aA_0K 2.js
@@ -0,0 +1,3 @@
+const t={date:"2024-05",title:"四轮与两轮辩论",url:"XXX",tags:["互动"]},n=`<p>四轮与两轮辩论，吐槽，互动，解忧。</p>
+`,o=`四轮与两轮辩论，吐槽，互动，解忧。
+`;export{t as attributes,n as html,o as markdown};
