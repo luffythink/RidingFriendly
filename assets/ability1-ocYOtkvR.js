638a2fdd478703abc7d5cdd4b3853c3ad14ab476
luffythink <luffythink@gmail.com>
2024-12-17 18:15:43 +0800

    updates

--- assets/ability1-ocYOtkvR.js
+++
@@ -1,7 +0,0 @@
-const t={date:"2024-08",Title:"无视交通规则怪",icon:"bi bi-1-circle-fill"},i=`<p>不遵守交通信号灯、不礼让行人等。</p>
-<div><img src="/assets/XXX" style="" title=""/></div>
-`,s=`不遵守交通信号灯、不礼让行人等。
-
-<div><img src="/assets/XXX" style="" title=""/></div>
-
-`;export{t as attributes,i as html,s as markdown};
---
+++ assets/daguai-BZQYC7xO 2.js
@@ -0,0 +1,3 @@
+const t={date:"2023-11",title:"沙包打怪",url:"XXX",tags:["怪物图鉴","打怪体验"]},n=`<p>一起标记一下，都打了什么“怪”。</p>
+`,o=`一起标记一下，都打了什么“怪”。
+`;export{t as attributes,n as html,o as markdown};
