474d21a0f220a07e3059c634985f9c6b387d3cf0
luffythink <luffythink@gmail.com>
2024-12-17 21:02:46 +0800

    updates

---
+++ assets/bottle-painting-t8DWTMiE 2.js
@@ -0,0 +1,4 @@
+const t={date:"2023-10",title:"水与祝福的共享",url:"XXX",tags:["共绘","互动"]},n=`<p>骑行补给，水瓶共创，骑行提示、姿势纸板造型展示，故事，画画、拍照。</p>
+`,o=`骑行补给，水瓶共创，骑行提示、姿势纸板造型展示，故事，画画、拍照。
+
+`;export{t as attributes,n as html,o as markdown};
---
+++ assets/road-book-NOwuxkxo 2.js
@@ -0,0 +1,3 @@
+const t={date:"2017-06-28",title:"骑行中",id:"road-book",company:"骑字与路书指南",range:"2017 - 2018",url:"XXX"},o=`<p>待完善。</p>
+`,n=`待完善。
+`;export{t as attributes,o as html,n as markdown};
