832abdb90c5414e9ca2aed8dc9a6af8657e25afa
luffythink <luffythink@gmail.com>
2024-12-17 20:32:49 +0800

    updates

---
+++ assets/nft-DvYumilb 2.js
@@ -0,0 +1,2 @@
+const t={date:"2024-02",title:"骑行 NFT",url:"XXx",tags:["体验","互动"]},n=`<p>NFT 数字徽章平替实物奖牌，打怪游戏机制设计。</p>
+`,o="NFT 数字徽章平替实物奖牌，打怪游戏机制设计。";export{t as attributes,n as html,o as markdown};
---
+++ assets/zaiye-spBzZD8N 3.js
@@ -0,0 +1,3 @@
+const t={date:"2023",title:"乡村骑行据点",url:"XXX",tags:["在野","美食"]},n=`<p>天星村骑行友好据点共创。</p>
+`,o=`天星村骑行友好据点共创。
+`;export{t as attributes,n as html,o as markdown};
