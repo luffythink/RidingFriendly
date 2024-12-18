ff9367a29ec5111a6164521400f3e4a488c2588b
luffythink <luffythink@gmail.com>
2024-12-18 19:31:52 +0800

    updates

---
+++ assets/riding-CM5Q6Kvi 2.js
@@ -0,0 +1,3 @@
+const t={date:"2024.11",title:"一起成都“修仙”骑",cover:"riding.jpg",url:"https://mp.weixin.qq.com/s/uKKLvdFDyTyrdMs21rhTRQ",tags:["户外骑行","城乡游走","骑遇","追风"]},n=`<p>有人说：“过去，人们骑行可能是为了更快地到达目的地；现在，我们骑行是为了更慢地融入城市与乡村自然当中。</p>
+`,r=`有人说：“过去，人们骑行可能是为了更快地到达目的地；现在，我们骑行是为了更慢地融入城市与乡村自然当中。
+`;export{t as attributes,n as html,r as markdown};
