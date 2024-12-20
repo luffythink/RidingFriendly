355cf9afcce1cfa364a9d5f4dce2fd0e50c98bca
luffythink <luffythink@gmail.com>
2024-12-20 21:02:06 +0800

    updates

--- assets/3nft-9jJbSFeq 3.js
+++
@@ -1,4 +0,0 @@
-const t={date:"2024-02",title:"骑行 NFT Mint",url:"https://et9v2rylce.feishu.cn/wiki/V1wTwb615iDfXakFbW5c6VeqnG7?",tags:["体验","互动"]},n=`<p>NFT 数字徽章平替实物奖牌，打怪游戏机制设计。By 城汇玩</p>
-`,e=`NFT 数字徽章平替实物奖牌，打怪游戏机制设计。By 城汇玩
-
-`;export{t as attributes,n as html,e as markdown};
--- assets/ability4-BGrngq7H 2.js
+++
@@ -1,4 +0,0 @@
-const t={date:"2024-05",Title:"逆行怪",icon:"bi bi-4-circle-fill"},n=`<p>不按照规定的骑行方向行驶。</p>
-`,i=`不按照规定的骑行方向行驶。
-
-`;export{t as attributes,n as html,i as markdown};
