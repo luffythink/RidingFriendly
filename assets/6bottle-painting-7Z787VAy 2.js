648d8b87aef421ccad8c5d9e7a87ad1aee555503
luffythink <luffythink@gmail.com>
2024-12-30 13:24:53 +0800

    updates

---
+++ assets/6bottle-painting-7Z787VAy 2.js
@@ -0,0 +1,3 @@
+const t={date:"2023-10",title:"水与祝福的共享",url:"https://et9v2rylce.feishu.cn/wiki/XQ3wwgvlriQj2dk4hX4coRlTnlg?",tags:["共绘","互动"]},n=`<p>欢迎来到串门骑行补给站！请享用水和祝福… By 青静</p>
+`,e=`欢迎来到串门骑行补给站！请享用水和祝福...... By 青静
+`;export{t as attributes,n as html,e as markdown};
