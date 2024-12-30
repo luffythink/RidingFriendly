98c933400e83f17e61aac2914d9c283b7595528e
luffythink <luffythink@gmail.com>
2024-12-30 10:56:57 +0800

    updates

--- assets/4cuowa-D-XeksX5 2.js
+++
@@ -1,4 +0,0 @@
-const t={date:"2024-01",title:"有爱的错袜骑行",url:"https://et9v2rylce.feishu.cn/wiki/KVG6wdqubiYVCYkc6QLcWzGlnFd?",tags:["公益","温暖同行"]},n=`<p>一起穿错袜，爱与包容同行。 By 善淘</p>
-`,c=`一起穿错袜，爱与包容同行。 By 善淘
-
-`;export{t as attributes,n as html,c as markdown};
--- assets/guai5-DxUBgzIo 3.js
+++
@@ -1,2 +0,0 @@
-const t={date:"2024-04",Title:"可持续生活理念",icon:"bi bi-emoji-neutral"},n=`<p>比如：对绿色出行重要性的认知不足、对骑行安全的忽视、对骑行友好的不重视等…</p>
-`,o="比如：对绿色出行重要性的认知不足、对骑行安全的忽视、对骑行友好的不重视等...";export{t as attributes,n as html,o as markdown};
