f49dc7d74797aef53f4e2b84c5b8d497ae53dc9a
luffythink <luffythink@gmail.com>
2025-01-06 12:16:01 +0800

    updates

---
+++ assets/handbook-Lp3D1-uq 3.js
@@ -0,0 +1,15 @@
+const n={date:"2019-06-28",title:"骑行前",id:"handbook",company:"骑行友好打怪手册",range:"2024-2025",url:"https://et9v2rylce.feishu.cn/wiki/VKi1wFCoxi54wUks1eecONHsnob?"},t=`<p>以实际行动支持并一起迭代：</p>
+<ul>
+<li><strong>安全骑行，生命至上</strong></li>
+<li><strong>文明骑行，绿色低碳</strong></li>
+<li><strong>有趣骑行，共享快乐</strong></li>
+</ul>
+<p>以实际行动践行安全、文明、有趣的骑行理念。详情点击@绿色标题查看。By 串门骑行打怪队</p>
+`,o=`以实际行动支持并一起迭代：
+- **安全骑行，生命至上**
+- **文明骑行，绿色低碳**
+- **有趣骑行，共享快乐**
+
+以实际行动践行安全、文明、有趣的骑行理念。详情点击@绿色标题查看。By 串门骑行打怪队
+
+`;export{n as attributes,t as html,o as markdown};
