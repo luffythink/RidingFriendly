4035066d697de3d55d2024498c1655977e3d2b87
Oscar@JING <luffythink@gmail.com>
2025-05-11 19:50:51 +0800

    updates

--- assets/0biketolife-DhfhHrOd.js
+++
@@ -1,4 +0,0 @@
-const a={date:"2026",title:"推广骑行的经济方案",url:"https://itdp.org/wp-content/uploads/2022/06/CN_%E6%8E%A8%E5%B9%BF%E9%AA%91%E8%A1%8C%E7%9A%84%E7%BB%8F%E6%B5%8E%E6%96%B9%E6%A1%88.pdf",tags:["可持续发展","自行车经济学"]},t=`<p>报告主要分为两部分，第 1 部分探讨自行车出行占比多大时能降低个人和社会成本，并为为城市 创造收入。此外分析了自行车数量和自行车出行需求的增加如何为社会创造就业和经济机遇；第 2 部分讨论了实现自行车经济效益最大化所需的投资类型和规模。By Dana Yanocha, ITDP &amp;  Sarah Mawdsley, ITDP</p>
-`,n=`报告主要分为两部分，第 1 部分探讨自行车出行占比多大时能降低个人和社会成本，并为为城市 创造收入。此外分析了自行车数量和自行车出行需求的增加如何为社会创造就业和经济机遇；第 2 部分讨论了实现自行车经济效益最大化所需的投资类型和规模。By Dana Yanocha, ITDP &  Sarah Mawdsley, ITDP
-
-`;export{a as attributes,t as html,n as markdown};
