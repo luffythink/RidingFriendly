2a663293fa0658b0a033a59c65114290a06632ba
Oscar@JING <luffythink@gmail.com>
2025-08-14 12:49:49 +0800

    updates

---
+++ assets/more-CxYDpaPb.js
@@ -0,0 +1,13 @@
+const n={date:"2009-06-28",title:"骑行前中后",id:"more",company:"更多在路上",range:"2025",url:"https://et9v2rylce.feishu.cn/wiki/M40mwo9aEi5qsBkjaK0c0GHtnGf?"},t=`<p>骑行友好，践行可持续生活方式：</p>
+<ul>
+<li><strong>自由发起</strong></li>
+<li><strong>文明发挥</strong></li>
+<li><strong>尽情 riding</strong> ♾️</li>
+</ul>
+`,i=`骑行友好，践行可持续生活方式：
+
+- **自由发起**
+- **文明发挥**
+- **尽情 riding** ♾️
+
+`;export{n as attributes,t as html,i as markdown};
